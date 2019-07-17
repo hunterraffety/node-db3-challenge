b78b4441f261efff6d381108b0b0dc9628337f37
hsrsbd <hsraffety@gmail.com>
2019-07-17 12:02:47 -0700

    database hooked up, api up and running. data coming back.

---
+++ schemes/scheme-model.js
@@ -0,0 +1,43 @@
+const db = require('../data/db-config');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+  return db('schemes')
+    .where({ id })
+    .first()
+    .then(scheme => {
+      if (scheme) {
+        return scheme;
+      } else {
+        return null;
+      }
+    });
+}
+
+function findSteps(id) {
+  //
+}
+
+function add(scheme) {
+  //
+}
+
+function update(changes, id) {
+  //
+}
+
+function remove(id) {
+  //
+}
